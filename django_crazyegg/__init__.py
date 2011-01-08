71d63fc6a5a8c7090b5729600f2b0f71f25179c0
Joost Cassee <joost@cassee.net>
2011-01-08 11:55:43 +0100

    Add changelog to the package docstring.
    Also changed README.txt to reStructuredText and added some more headers
    to the package documentation.

--- django_crazyegg/__init__.py
+++ django_crazyegg/__init__.py
@@ -1,4 +1,5 @@
 """
+================================
 Crazy Egg integration for Django
 ================================
 
@@ -10,7 +11,7 @@ Django_ project.
 
 
 Overview
---------
+========
 
 Crazy Egg is an easy to use hosted web application that generates
 heatmaps from webpage visitor clicks.  It allows you to discover the
@@ -20,7 +21,7 @@ Crazy Egg very simple.
 
 
 Installation and configuration
-------------------------------
+==============================
 
 To install django-crazyegg, simply place the ``django_crazyegg``
 package somewhere on the Python path.  The application is configured in
@@ -41,12 +42,20 @@ setting::
 
 
 Usage
------
+=====
 
-In your template, load the django-crazyegg template tags by adding
-``{% load crazyegg %}`` at the top of the template.  Then use the
-``track_crazyegg`` tag to insert the Javascript tracking code.  The
-Crazy Egg web pages recommend adding the code directly before the
+The django-crazyegg application provides two template tags. One to track
+visitor clicks, and one to register user variables. In order to use the
+tags in a template, first load the django-crazyegg them by adding
+``{% load crazyegg %}`` at the top.
+
+
+Tracking visitor clicks
+-----------------------
+
+Crazy Egg uses Javascript to track every visitor click. The
+``track_crazyegg`` tag inserts the tracking code into the HTML page.
+The Crazy Egg web pages recommend adding the code directly before the
 closing ``</body>`` HTML tag::
 
         ...
@@ -54,14 +63,21 @@ closing ``</body>`` HTML tag::
     </body>
     </html>
 
-*Note:* Versions of django-crazyegg prior to 2.0.0 used asynchronous
-loading to allow the tag to be added to the HTML head section.
-Unfortunately, that caused problems if you wanted to set user variables
-(see below).
+.. note::
+
+    Versions of django-crazyegg prior to 2.0.0 used asynchronous
+    loading to allow the tag to be added to the HTML head section.
+    Unfortunately, that caused problems if you wanted to set user
+    variables (see below).
 
 Even if you only track clicks on a specific page, you can still insert
-the tracking tag in your base template.  Crazy Egg will only install the
-Javascript event handler on URLs that you have created snapshots for.
+the tracking tag into your base template.  The code will only install
+the Javascript event handler on URLs that you have created snapshots
+for.
+
+
+User variables
+--------------
 
 Crazy Egg can segment clicks based on `user variables`_.  If you want to
 set a user variable, use the ``set_uservar`` tag.  It takes two
@@ -78,6 +94,26 @@ multiple times::
 
 .. _`user variables`: https://www.crazyegg.com/help/Setting_Up_A_Page_to_Track/How_do_I_set_the_values_of_User_Var_1_User_Var_2_etc_in_the_confetti_and_overlay_views/
 
+
+Changelog
+=========
+
+1.0.0
+    Project created from code used in the `IPv6 Ready`_ project.
+
+1.0.1
+    Fixed links to the Github project pages in the ``setup.py`` script.
+
+2.0.0
+    Added the `set_uservar` template tag to set Crazy Egg user
+    variables.  These can be used to segment clicks on the confetti and
+    layout views.
+
+    Because variables can only be set after the tracking code has been
+    loaded, the tracking template tag has been reverted to the code that
+    Crazy Egg recommends and is no longer asynchronous.
+
+.. _`IPv6 Ready`: http://www.ipv6ready.nl/
 
 ------------------------------------------------------------------------
 
